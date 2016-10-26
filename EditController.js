dbb8bda91c21db14ab81588eac0873c91637e0c4
Ubanam <watanabemanabu@watanabe-no-MacBook-Air.local>
2016-10-26 09:15:00 +0900

    初回作成
    ページリニューアル

---
+++ EditController.js
@@ -0,0 +1,57 @@
+var myApp = angular.module('meigenizm',['ngResource']);
+
+myApp.factory('movies', ['$resource', function($resource){
+	var movies = $resource('/api/movies/:id/:Title/:Type/:data' , {id:'@id', Title:'@Title'}, {
+		get: {method: 'GET', isArray: true},
+		query: {method: 'GET', isArray: true},
+		save: {method: 'POST'},
+		remove: {method: 'DELETE'}
+	});
+	return movies;
+}]);
+
+///////////////////////////////////
+//EditController
+///////////////////////////////////
+myApp.controller('EditController',['$scope','$http','movies', function ($scope, $http, movies){ 
+	//Get the all data of movies
+	var movieList = {};
+	$scope.movies = movies.query();
+	$scope.detailFlg = false;
+	$scope.detail = {};
+	
+
+///////////////////////////////////
+//Data CRUD
+///////////////////////////////////
+  //Update Data
+  $scope.updateData = function(data) {
+    console.log(data._id);
+    $http.put('/api/movies/'+data._id, data).success(function(data) {
+      console.log(data);	
+    });
+  };
+  
+  //Edit Data
+  $scope.editData = function(data){
+    $scope.data = data;
+  }
+    
+  //add Data  
+  $scope.addData = function(){
+    $http.put('/api/movies/').success(function(data){
+      console.log(data);
+    })
+  }	
+}]);
+
+////////////////////////////////
+//Modal Controller
+////////////////////////////////
+var ModalController = function ($scope, $modalInstance, detail) {
+	$scope.detail = detail;
+	//close the modal
+	$scope.closeModal = function () {
+		$modalInstance.dismiss('cancel');
+	};
+};
---
+++ appController.js
@@ -0,0 +1,91 @@
+var myApp = angular.module('meigenizm',['ngRoute','ngResource']);
+
+//////////////////////////////////////////
+//Setting Factory
+//////////////////////////////////////////
+myApp.factory('movies', ['$resource', function($resource){
+	var movies = $resource(
+	  '/api/movies/:query', 
+	  {query:'@query'}, {
+		get: {method: 'GET',isArray:true},
+		query: {method: 'GET', isArray: true},
+		select:{method: 'POST',isArray:true},
+		save: {method: 'POST'},
+		remove: {method: 'DELETE'}
+	});
+	return movies;
+}]);
+
+myApp.config(['$routeProvider', function($routeProvider,data) {
+	$routeProvider
+    .when('/index/', { controller:'IndexController', templateUrl: 'main.html' }) // templateUrlでテンプレートとなるファイルとテンプレ名を指定
+    .when('/detail/', { controller:'DetailController', templateUrl: 'detail.html' }) // 同様にテンプレ指定（ここでは遷移後）
+    .when('/about/', { controller:'AboutController', templateUrl: 'about.html' }) // 同様にテンプレ指定（ここでは遷移後）
+    .when('/list/', { controller:'IndexController', templateUrl: 'list.html' }) // 同様にテンプレ指定（ここでは遷移後）
+    .otherwise({redirectTo: '/index/'}); // 初めに表示するテンプレ名
+		return data;
+  }]);
+
+myApp.factory('shareData', function(){
+	shareData = {}
+	return shareData;	
+});
+
+myApp.controller('IndexController',['$scope', '$http', '$location','$resource','movies', 'shareData', function ($scope, $http, $location, $resource, movies, shareData ){ 
+/////////////////////////////////////////////////
+//初期設定
+/////////////////////////////////////////////////
+	//Get the movielist data
+	$scope.movies = movies.query();
+
+/////////////////////////////////////////////////
+//各ページへ遷移
+/////////////////////////////////////////////////
+	//詳細ページ
+	$scope.moveDetail = function(data){
+		shareData.data =data;
+		$location.path('/detail/');
+		console.log(data);
+	}
+	//メイゲニズムについて
+	$scope.moveAbout = function(){
+		$location.path('/about/');
+	}
+
+	
+  //End of Controller	
+}]);
+
+myApp.controller('DetailController',['$scope', '$http','$resource','$location','movies', 'shareData', function ($scope, $http, $resource, $location, movies, shareData ){ 
+/////////////////////////////////////////////////
+//初期設定
+/////////////////////////////////////////////////
+	//Get the movielist data
+	$scope.movies = movies.query();
+	$scope.data = shareData.data;
+
+	//linkへの遷移
+	$scope.moveLink = function(link){
+		console.log("link:"+link);
+		$location.href = link;
+	}
+  //End of Controller	
+	
+}]);
+
+myApp.controller('AboutController',['$scope', '$http','$resource','$location','movies', 'shareData', function ($scope, $http, $resource, $location, movies, shareData ){ 
+/////////////////////////////////////////////////
+//初期設定
+/////////////////////////////////////////////////
+	//Get the movielist data
+	$scope.movies = movies.query();
+	$scope.data = shareData.data;
+
+	//linkへの遷移
+	$scope.moveLink = function(link){
+		console.log("link:"+link);
+		$location.href = link;
+	}
+  //End of Controller	
+	
+}]);
